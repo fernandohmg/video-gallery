b682910ab66d7047bc3f748b48f197849c53cc90
Fernando Goncalves <fernando.hmg@gmail.com>
2023-07-30 22:17:22 -0300

    chore: enabling cors and validating video post endpoint

--- backend/src/video/dto/create-video.dto.ts
+++ backend/src/video/dto/create-video.dto.ts
@@ -1,4 +1,19 @@
+import { IsNotEmpty, IsString, Matches, MaxLength } from 'class-validator';
+
 export class CreateVideoDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
+  @Matches(
+    /(?:https:\/\/www\.youtube\.com\/watch\?v=|https:\/\/youtu\.be\/)([a-zA-Z0-9_-]{11})/,
+    {
+      message: 'url must be a valid youtube url',
+    },
+  )
   url: string;
 }
